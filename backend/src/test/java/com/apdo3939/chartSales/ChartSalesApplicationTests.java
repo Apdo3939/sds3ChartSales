e5e9b7042c38a50a97997e872996e102ac654d53
Apdo3939 <apdo3939@hotmail.com>
2021-05-03 10:37:19 -0300

    Project created

---
+++ backend/src/test/java/com/apdo3939/chartSales/ChartSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apdo3939.chartSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
