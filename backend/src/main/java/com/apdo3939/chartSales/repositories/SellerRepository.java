5eed6517eaad1c4977785167dd19e112a2af99c0
Apdo3939 <apdo3939@hotmail.com>
2021-05-05 15:33:36 -0300

    First homolog

--- backend/src/main/java/com/apdo3939/chartSales/repositories/SellerRepository.java
+++ backend/src/main/java/com/apdo3939/chartSales/repositories/SellerRepository.java
@@ -3,7 +3,6 @@ package com.apdo3939.chartSales.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.apdo3939.chartSales.entities.Sale;
 import com.apdo3939.chartSales.entities.Seller;
 
 @Repository
