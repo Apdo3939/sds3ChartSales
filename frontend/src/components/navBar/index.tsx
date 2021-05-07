6a33a3f6dd4bc62f6199e915d03406945e0ebcbf
Apdo3939 <apdo3939@hotmail.com>
2021-05-07 10:22:06 -0300

    Home navigation

--- frontend/src/components/navBar/index.tsx
+++ frontend/src/components/navBar/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-primary border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <h1 className="text-light">Sales Chart</h1>
+                    <Link to="/">
+                        <h1 className="text-light">Sales Chart</h1>
+                    </Link>
                 </nav>
             </div>
         </div>
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,10 +1,25 @@
+import Footer from "components/footer";
+import NavBar from "components/navBar";
+import { Link } from "react-router-dom";
 
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4 text-primary">Chart Sales</h1>
+                    <p className="lead text-primary">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p className="text-primary">Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                </div>
+                <Link to="/dashboard" className="btn btn-primary btn-lg">
+                    Acessar o dashBoard
+                </Link>
+            </div>
+            <Footer />
+        </>
     );
 }
 
